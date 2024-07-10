e855d999f76b9f6713f49fa71065826f9cb35961
Jethro Reuel Chika <163969954+JethroReuel16@users.noreply.github.com>
2024-07-10 09:37:48 +0200

    added some colors

--- app/Mercanto/app/(tabs)/Products.tsx
+++ app/Mercanto/app/(tabs)/Products.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { green } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Mercato Your No. 1 Store</Text>
+      <Text style={styles.header}>Mercato</Text>
       <View style={styles.container}>
       
-      <MaterialIcons name="shopping-cart" size={30} color="#000" />
+      <MaterialIcons name="shopping-cart" size={30} style={{color:'#BEF264', position:'absolute',marginHorizontal: 5, marginVertical: 25 }}/>
+      
     </View>
       <TextInput
         style={styles.searchInput}
+        placeholderTextColor={'gray'}
         placeholder="search for a product"
       />
 
@@ -19,17 +23,17 @@ const App = () => {
 
 
 
-      <View style={styles.filterContainer}>
-        <TouchableOpacity style={styles.filterButton}><Text>All</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.filterButton}><Text>Clothings</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.filterButton}><Text>Electronic</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.filterButton}><Text>Books</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.filterButton}><Text>shoes</Text></TouchableOpacity>
+      <View style={styles.filterContainer }>
+        <TouchableOpacity style={styles.filterButton}><Text style={{color:'#FFFFFF'}}>All</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterButton}><Text  style={{color:'#FFFFFF'}}>Clothings</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterButton}><Text  style={{color:'#FFFFFF'}}>Electronic</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterButton}><Text  style={{color:'#FFFFFF'}}>Books</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.filterButton}><Text  style={{color:'#FFFFFF'}}>shoes</Text></TouchableOpacity>
       </View>
       <View style={styles.productsContainer}>
         {Array.from({ length: 20 }).map((_, index) => (
           <View key={`product-${index}`} style={styles.productItem}>
-            <Text>ZMK 0.00 </Text>
+            <Text style={{color:'#FFFFFF'}}>ZMK 0.00 </Text>
           </View>
         ))}
       </View>
@@ -40,29 +44,34 @@ const App = () => {
 const styles = StyleSheet.create({
 
   
-
-
+  
 
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#fff',
-    borderRadius: 20,
+    backgroundColor: '#09090B',
+    
      
   },
   header: {
-    fontSize: 30,
+    fontSize: 25,
     fontWeight: 'bold',
     textAlign: 'left',
-    marginVertical: 20,
+    marginVertical: 3,
+    marginHorizontal: 4,
+    marginBottom: -6,
+    color : '#FB923C',
+    borderBlockColor: 'BEF264'
   },
   searchInput: {
     height: 30,
     width: '45%',
-    borderColor: 'gray',
-    borderWidth: 1,
+    borderColor: '#FB923C',
+    borderWidth: 2,
     paddingHorizontal: 5,
     borderRadius: 5,
+    marginHorizontal: 170,
+    
   },
   filterContainer: {
     flexDirection: 'row',
@@ -73,33 +82,31 @@ const styles = StyleSheet.create({
   filterButton: {
     paddingVertical: 5,
     paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
+    borderWidth: 2,
+    borderColor: '#FB923C',
     justifyContent: 'space-around',
-    borderRadius: 5,
+    borderRadius: 6,
+  
   },
   productsContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
     
   },
   productItem: {
     width: '45%',
     height: 150,
-    borderWidth: 1,
-    borderColor: 'gray',
+    borderWidth: 3,
+    borderColor: '#FB923C',
     marginVertical: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 8,
+    borderRadius: 10,
+    
     
   },
-  addButton: {
-    padding: 1,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 8,
-  },
+  
 });
 
 export default App;
